230f8e3b390966cd102bd7b8080d8c5a51e3cc0f
Brice AIELLO <brice.aiello@me.com>
2020-05-26 19:26:48 +0200

    feat: add cors and helmet headers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const helmet = require('helmet')
+
+const corsMiddleware = require('./middlewares/corsMiddleware')
 
 require('dotenv').config()
 const PORT = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
+
+app.use(helmet())
+app.use(corsMiddleware)
 
 app.get('/', (req, res) => res.send('Hello BAPI'))
 
