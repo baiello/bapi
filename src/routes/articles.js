6259f7af0d78eb4f373c7668241bd8bd18f2c5ee
Brice AIELLO <brice.aiello@me.com>
2020-05-27 10:39:10 +0200

    feat: fetch articles from db

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const mongodb = require('mongodb')
+
+const { getDb } = require('../utils/db')
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('Articles list'))
+router.get('/', (req, res) => {
+  const db = getDb()
+  return db
+    .collection('articles')
+    .find()
+    .toArray()
+    .then(articles => res.json(articles))
+    .catch(err => {
+      throw new Error(err)
+    })
+})
 
-router.get('/:slug', (req, res) => res.send(`Article ${req.params.slug}`))
+router.get('/:id', (req, res) => {
+  const db = getDb()
+  return db
+    .collection('articles')
+    .find({ _id: mongodb.ObjectId(req.params.id) })
+    .next()
+    .then(article => res.json(article))
+    .catch(err => {
+      throw new Error(err)
+    })
+})
 
 module.exports = router
